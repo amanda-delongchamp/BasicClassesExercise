d46573af027470ab0da613138054af18b8ff1327
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-05-16 08:19:25 -0700

    Add project files.

---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myNewCar = new Car();
+
+            myNewCar.Make = "Ford";
+            myNewCar.Model = "Expedition";
+            myNewCar.Year = 2021;
+
+            Console.WriteLine($"My new car is a {myNewCar.Year} {myNewCar.Make} {myNewCar.Model}");
+        }
+    }
+}
